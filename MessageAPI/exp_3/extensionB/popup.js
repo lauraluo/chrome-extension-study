d171a522ecdc407d86d16d304a056cd9b4774033
apple <apple@appledeiMac.local>
2016-12-28 19:32:05 +0800

    no message

--- MessageAPI/exp_3/extensionB/popup.js
+++ MessageAPI/exp_3/extensionB/popup.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(dcle) {
     var dButton = document.getElementById("button");
+    //注意擴充功能A的ID在開始狀態是會變的，如果你要在你的地端跑這個範例
+    //請自行在擴充功能管理頁面查看ID
+    //一旦正式發佈你的擴充功能，你可以得到一組固定ID
     var extensionID = "fhggngpimllbngfbjdbiplgmhdppiiop";
 
-    //點擊按鈕，自擴充功能A發動訊息
+    //點擊按鈕，向擴充功能A發動訊息
     dButton.addEventListener('click', function(e) {
 
         console.log("click");
